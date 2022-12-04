de34a366d062f342af9b6b44356054fa68b2b515
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2022-12-04 11:52:08 +0100

    day 3 part 2 done

--- day_3/main.py
+++ day_3/main.py
@@ -1,5 +1,27 @@
 input_file = open("input/input.txt", mode="r")
 result = 0
+group = []
+
+
+def find_group_duplicates(group):
+    items = []
+    duplicates = []
+
+    rucksack_1 = group[0]
+    rucksack_2 = group[1]
+    rucksack_3 = group[2]
+
+    for item in rucksack_1:
+        if not items.__contains__(item):
+            items.append(item)
+
+    for item in rucksack_2:
+        if items.__contains__(item):
+            duplicates.append(item)
+
+    for item in rucksack_3:
+        if duplicates.__contains__(item):
+            return item
 
 
 def calc_priority(duplicate_item):
@@ -9,15 +31,12 @@ def calc_priority(duplicate_item):
         return ord(duplicate_item) - ord("A") + 27
 
 
-for line in input_file.readlines():
-    items = list(line)[:len(line) - 1]
-    first_half = items[:len(items) // 2]
-    second_half = items[len(items) // 2: len(items)]
-
-    for item in first_half:
-        if second_half.__contains__(item):
-            print(item, calc_priority(item))
-            result += calc_priority(item)
-            break
+for counter, line in enumerate(input_file.readlines()):
+    group.append(list(line.strip()))
+    if (counter + 1) % 3 == 0:
+        badge = find_group_duplicates(group)
+        print(badge, calc_priority(badge))
+        result += calc_priority(badge)
+        group = []
 
 print(result)
